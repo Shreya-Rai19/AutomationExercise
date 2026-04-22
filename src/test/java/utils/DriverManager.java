348bbfed73a3907e630b5c3969dc5bd93eabbe30
Mansi Naik <mansi.naik@mphasis.com>
2026-04-22 12:46:42 +0530

    Valid user login

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -1,7 +1,6 @@
 package utils;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
@@ -9,27 +8,29 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import pages.HomePage;
-
+import pages.LoginPage;
 
 public class DriverManager {
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected ConfigReader config = new ConfigReader();
-    
+
     protected HomePage homepage;
-    
+    protected LoginPage loginpage;
+
     @BeforeClass
     public void setup() {
         config = new ConfigReader();
-        driver = DriverFactory.getDriver(config.getBrowser());
+        driver = DriverFactory.getDriver(config.getBrowser()); // launches Chrome
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get(config.getBaseUrl());
+        driver.get(config.getBaseUrl()); // https://automationexercise.com/
     }
 
     @BeforeMethod
     public void instantiatePages() {
-    	homepage = new HomePage(driver,wait);
+        homepage = new HomePage(driver, wait);
+        loginpage = new LoginPage(driver, wait);
     }
 
     @AfterSuite
